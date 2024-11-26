6ff18f1ada55969816cc9a2f19d0e8ec582ec273
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-11-26 16:40:23 +0400

    uibuild

--- backend/index.js
+++ backend/index.js
@@ -1,44 +1,46 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const morgan = require("morgan");
+const morgan = require('morgan');
+const cors = require('cors');
 
 let phonebook = [
   {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
+    id: '1',
+    name: 'Arto Hellas',
+    number: '040-123456',
   },
   {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
+    id: '2',
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
   },
   {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
+    id: '3',
+    name: 'Dan Abramov',
+    number: '12-43-234345',
   },
   {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
+    id: '4',
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
   },
 ];
 
-morgan.token("content", function (req, res) {
+morgan.token('content', function (req, res) {
   console.log(req.body);
 
   return JSON.stringify(req.body);
 });
 
 const morganLogger = morgan(
-  ":method :url :status :res[content-length] - :response-time ms :content"
+  ':method :url :status :res[content-length] - :response-time ms :content'
 );
 
+app.use(express.static('build'));
 app.use(express.json());
 app.use(morganLogger);
 
-const cors = require("cors");
+const cors = require('cors');
 
 app.use(cors());
 
@@ -47,22 +49,22 @@ const generateId = () => {
   return id;
 };
 
-app.get("/api/persons", (request, response) => {
+app.get('/api/persons', (request, response) => {
   response.json(phonebook);
 });
 
-app.get("/api/persons/:id", (request, response) => {
+app.get('/api/persons/:id', (request, response) => {
   const id = String(request.params.id);
   const person = phonebook.find((person) => person.id === id);
   if (person) {
     response.json(person);
   } else {
-    console.log("not found");
+    console.log('not found');
     response.status(404).end();
   }
 });
 
-app.get("/info", (request, response) => {
+app.get('/info', (request, response) => {
   const phonebookLength = phonebook.length;
   const date = new Date();
   response.send(
@@ -70,24 +72,24 @@ app.get("/info", (request, response) => {
   );
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
   const id = String(request.params.id);
   phonebook = phonebook.filter((p) => p.id !== id);
   response.status(204).end();
 });
 
-app.post("/api/persons", (request, response) => {
+app.post('/api/persons', (request, response) => {
   const body = request.body;
 
   if (!body.name || !body.number) {
     return response.status(400).json({
-      error: "content missing",
+      error: 'content missing',
     });
   }
 
   const duplicate = phonebook.find((person) => body.name === person.name);
   if (duplicate) {
-    return response.status(400).json({ error: "name must be unique" });
+    return response.status(400).json({ error: 'name must be unique' });
   }
 
   const newPerson = {
