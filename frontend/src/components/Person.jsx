d4fea23915741a73ccec5c4f8ea04df0cd02841b
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-11-28 03:09:31 +0400

    Removed frontend

--- frontend/src/components/Person.jsx
+++
@@ -1,8 +0,0 @@
-const Person = ({ name, number, deletePerson }) => (
-  <li>
-    {name} {number}
-    {deletePerson ? <button onClick={deletePerson}>delete</button> : null}
-  </li>
-);
-
-export default Person;
